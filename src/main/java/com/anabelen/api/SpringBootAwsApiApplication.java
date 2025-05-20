988ec7564a07082f12b489bdb9b6a026b3fa4fa3
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-05-20 12:26:16 +0200

    Primer commit: API Spring Boot para AWS

---
+++ src/main/java/com/anabelen/api/SpringBootAwsApiApplication.java
@@ -0,0 +1,13 @@
+package com.anabelen.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anabelen/api/SpringBootAwsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabelen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
